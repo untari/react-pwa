9feb4abfc2273c69dc57b4031abb85d047bc9a04
untari <sastraembun0306@gmail.com>
2021-03-30 21:26:22 +0700

    modified the component name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import SystemData from './components/SystemData';
-import SystemDiagnosticsManager  from './components/SystemDiagnosticsManager ';
-import SystemState  from './components/SystemState ';
+import SystemDiagnosticsManager from './components/SystemDiagnosticsManager';
+import SystemState from './components/SystemState';
 import Navigation from './components/Navigation';
  
 class App extends Component {
@@ -13,8 +13,8 @@ class App extends Component {
           <Navigation />
             <Switch>
              <Route path="/" component={SystemData} exact/>
-             <Route path="/SystemDiagnosticsManager " component={SystemDiagnosticsManager }/>
-             <Route path="/systemstate " component={SystemState }/>
+             <Route path="/systemdiagnosticsmanager " component={SystemDiagnosticsManager }/>
+             <Route path="/systemstate " component={SystemState}/>
            </Switch>
         </div> 
       </BrowserRouter>
